2e7d6ef02d0ea034233d97726e2e9e2f7567be5a
W0nda <luclemauff@outlook.fr>
2024-02-26 23:43:00 +0100

    optimization of requests preparation

--- readgps.c
+++ readgps.c
@@ -12,6 +12,13 @@ void flush(char* array, int size){
 	}
 }
 
+void selstr(char* array, int start, int n){
+	for (int i=0; i<n; i++) {
+		array[i] = array[start+i];
+	}
+	array[n]='\0';
+}
+
 int main(){
 	//GPS
 	int fd = open("/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_7_-_GPS_GNSS_Receiver-if00",O_RDONLY);
@@ -23,24 +30,25 @@ int main(){
 	if (PQstatus(dbConn) == CONNECTION_OK) {
 		printf("Pizzaaa!!\n");
 	}
-	char request[200]; 
+	char request[200];
+	PGresult* res;
 
 	while(1) {
 		strcpy(request,"INSERT INTO coordinates(lat,latcard,lon,loncard,time) VALUES (");
+		//table a changer dans DB: FLOAT4 -> FLOAT5
 		read(fd,&buffer,100);
 		if(strstr(buffer, "$GPGLL,")){
-			substr = strtok (buffer,",");
-  			for(int i=0;i<6;i++)
-  			{
-				if(i!=0){
-    					printf ("%s\n",substr);
-					strcat(request,substr);
-					strcat(request,",");
-				}
-    				substr = strtok (NULL, ",");
-  			}
+			printf("%s\n",buffer);
+			selstr(buffer, 7, 36);
+			printf("%s\n",buffer);
+			strcat(request, buffer);
 			strcat(request,");\n");
 			printf("Requests: %s\n",request);
+			res = PQexec(dbConn, request); //don't work(prbm de guillemet pour str)
+			if (PQresultStatus(res) == PGRES_COMMAND_OK){
+				printf("Ooooh yeah!!");
+			}
+			PQclear(res);
 			flush(request,sizeof(request));
 		}
 		flush(buffer,sizeof(buffer));
