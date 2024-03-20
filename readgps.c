655ab1991a8d27fd9dc81d060ae631f535633199
W0nda <luclemauff@outlook.fr>
2024-03-20 20:59:55 +0100

    everything change

--- readgps.c
+++ readgps.c
@@ -1,85 +1,39 @@
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
 
-//PostgreSQL lib
-#include <libpq-fe.h>
-
-void flush(char* array, int size){
-	for(int i=0; i<size; i++){
-		array[i] = 0x0;
+void frame2datas(char* frame, char datas[][20]){
+	int j=0; //put 7
+	printf("%s\n",frame);
+	for (int i=0; i<6; i++){
+		while (frame[j] != ',') {
+			datas[i][j] = frame[j];
+			j++;
+		}
+		datas[i][j+1]='\n';
+		j++;
+		printf("frame2datas %s\n",datas[i]);
 	}
-}
-
-void selstr(char* array){
-	for (int i=0; i<11; i++){
-		array[i]=array[7+i];
-	}
-
-	array[11]='\''; // 'N'
-	array[12]=array[18];
-	array[13]='\'';
-	//OK
-
-	for(int i=14; i<27; i++){
-		array[i]=array[5+i];
-	}
-
-	array[27]='\''; // 'W'
-	array[28]=array[32];
-	array[29]='\'';
-
-	for(int i=30; i<40; i++){
-		array[i]=array[3+i];
-	}
-
-	array[40]='\0';
 }
 
 int main(){
-	//GPS
-	int fd = open("/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_7_-_GPS_GNSS_Receiver-if00",O_RDONLY);
-	char buffer[100];
-	char* substr;
 
-	//DB
-	PGconn* dbConn = PQconnectdb("user=chirpstack password=chirpstack dbname=chirpstack");
-	if (PQstatus(dbConn) == CONNECTION_OK) {
-		printf("Pizzaaa!!\n");
-	}
-	char request[200];
-	PGresult* res;
+	FILE* gps = fopen("/dev/serial/by-id/usb-u-blox_AG_-_www.u-blox.com_u-blox_7_-_GPS_GNSS_Receiver-if00", "r");
+	char frame[100];
+	char datas[6][20];
 
-	while(1) {
-		strcpy(request,"INSERT INTO coordinates(lat,latcard,lon,loncard,time) VALUES (");
-		read(fd,&buffer,100);
-		if(strstr(buffer, "$GPGLL,")){
-			//preparing buffer
-			printf("%s\n",buffer);
-			selstr(buffer);
-			printf("%s\n",buffer);
-
-			//preparing request
-			strcat(request, buffer);
-			strcat(request,");\n");
-			printf("Requests: %s\n",request);
-
-			res = PQexec(dbConn, request); //don't work(prbm de guillemet pour str)
-			if (PQresultStatus(res) == PGRES_COMMAND_OK){
-				printf("Ooooh yeah!!");
-			}else{
-				printf("%s\n",PQerrorMessage(dbConn));
+	for(int i=0; i<100; i++){
+		fgets(frame, sizeof(frame), gps);
+		if (strstr(frame,"$GPGLL")) {
+			printf("%s",frame);
+			frame2datas(frame,datas);
+			for (int j=0; j<6; j++) {
+				printf("%d\n",j);
+				printf("%s",datas[j]);
 			}
-
-			PQclear(res);
-			flush(request,sizeof(request));
+			printf("%c",'\n');
 		}
-		flush(buffer,sizeof(buffer));
 	}
 
-	PQfinish(dbConn);
-	close(fd);
-	
+	fclose(gps);
 	return 0;
 }
